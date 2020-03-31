8b1c6b88ea4ef22c3560942ab70235ed648b9ffa
Madhu Samala <madhusamala.trainer@gmail.com>
2020-03-31 23:50:49 +0530

    my-favourite-movies-assignment-boilerplate

---
+++ js/index.js
@@ -0,0 +1,35 @@
+const MOVIES_BACKEND_URL = "http://localhost:3000/movies";
+const FAV_MOVIES_BACKEND_URL = "http://localhost:3000/favourites";
+let moviesList = [];
+let favMoviesList = [];
+let errorMessage;
+
+/*
+   This function should call getMoviesFromBackend() 
+   and store that in an the global array 
+*/
+
+function getMovies() {
+    
+
+}
+
+/*
+   This function should fetch the movies from backend using Fetch API
+   if any error occured, this function should handle using Promise.reject()
+*/
+
+function getMoviesFromBackend() {
+  
+}
+
+/*
+   This function should display the given movies list on to the html page in the following element
+   <section class="movies-container" id="moviesContainer">
+
+*/
+
+function displayMoviesToHtml(moviesList) {
+    
+}
+
