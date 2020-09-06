b165b476cd713263cee0577978f4c2e7ddfe67fc
srichakradhar <srichakra3nsr3@gmail.com>
2020-09-06 05:39:43 -0400

    assignment 2 submission

--- js/index.js
+++ js/index.js
@@ -9,9 +9,17 @@ let errorMessage;
    and store that in an the global array 
 */
 
-function getMovies() {
-    
-
+async function getMovies(favs) {
+    if (favs) {
+        let url = 'http://localhost:3000/favourites/';
+        let res = await fetch(url);
+        favMoviesList = await res.json();
+        favMovieIds = favMoviesList.map(movie => movie.id);
+        url = 'http://localhost:3000/movies?id=' + favMovieIds.join('&id=');
+        res = await fetch(url);
+        moviesList = await res.json();
+    }
+    getMoviesFromBackend();
 }
 
 /*
@@ -19,8 +27,18 @@ function getMovies() {
    if any error occured, this function should handle using Promise.reject()
 */
 
-function getMoviesFromBackend() {
-  
+async function getMoviesFromBackend(query) {
+    if (favMoviesList.length == 0) {
+        try {
+            let url = 'http://localhost:3000/movies' + (query ? ('?q=' + query) : '');
+            let res = await fetch(url);
+            moviesList = await res.json();
+        } catch (error) {
+            console.log(error);
+            $('#errorMessage').text("Couldn't load JSON!");
+        }
+    }
+    displayMoviesToHtml(moviesList);
 }
 
 /*
@@ -29,7 +47,50 @@ function getMoviesFromBackend() {
 
 */
 
-function displayMoviesToHtml(moviesList) {
-    
+async function displayMoviesToHtml(moviesList) {
+
+    let movies = '';
+    let url = 'http://localhost:3000/favourites/';
+    let res = await fetch(url);
+    favMoviesList = await res.json();
+    favMovieIds = favMoviesList.map(movie => movie.id);
+
+    $.each(moviesList, function(i, movie) {
+
+        movies += '<div class="card movie_card" id=' + movie.id + '>' +
+            '<img src="' + movie.posterImageUrl + '" class="card-img-top" alt="' + movie.title + '">' +
+            '<div class="card-body">' +
+            '<i class="fa fa-heart fav_button' + (favMovieIds.includes('' + movie.id) ? ' favourite' : '') + '" data-toggle="tooltip" data-placement="bottom" title="Favourite"></i>' +
+            '<h5 class="card-title">' + movie.title + '</h5>' +
+            '<p class="card-text">' + movie.description + '</p>' +
+            '<span class="movie_info"><i class="fa fa-user"></i> ' + movie.director + '</span>' +
+            '<span class="movie_info float-right"><i class="fa fa-star"></i> ' + movie.rating + '</span></div> </div>';
+
+    });
+
+    $('#moviesList').html(movies);
+    $('[data-toggle="tooltip"]').tooltip()
+    $('.fav_button').on('click', async function() {
+        fav_button = $(this);
+        let id = fav_button.parent().parent().attr('id');
+        if (fav_button.hasClass('favourite')) {
+            $.ajax({
+                url: url + id,
+                type: 'DELETE',
+                success: function(result) {
+                    console.log('Removed movie ' + id + ' from favourites.\n', result);
+                }
+            });
+        } else {
+            $.post(url, { id: id }, function(data) {
+                console.log(data);
+            });
+        }
+        fav_button.toggleClass('favourite');
+    });
 }
 
+$('#search').click(function() {
+    let query = $('#query').val();
+    getMoviesFromBackend(query);
+});
